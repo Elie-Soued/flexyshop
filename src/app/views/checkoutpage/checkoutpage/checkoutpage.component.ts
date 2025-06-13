28cee7b39786cd90c826aada231407b148c7db82
Elie Soueid <eliegebransoued@gmail.com>
2025-06-13 12:30:06 +0200

    creating the checkout page

---
+++ src/app/views/checkoutpage/checkoutpage/checkoutpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkoutpage',
+  imports: [],
+  templateUrl: './checkoutpage.component.html',
+  styleUrl: './checkoutpage.component.css'
+})
+export class CheckoutpageComponent {
+
+}
