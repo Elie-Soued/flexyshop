54244ae07e4cde51abf5e8ee9d783e00ec288b19
Elie Soueid <eliegebransoued@gmail.com>
2025-06-19 09:48:21 +0200

    test cases for checkoutpate

--- src/app/views/checkoutpage/checkoutpage.component.spec.ts
+++ src/app/views/checkoutpage/checkoutpage.component.spec.ts
@@ -1,29 +1,62 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CheckoutpageComponent } from './checkoutpage.component';
-import { provideStore } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import { provideRouter } from '@angular/router';
-import { cartReducer } from '../../store/store.reducer';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('CheckoutpageComponent', () => {
   let component: CheckoutpageComponent;
   let fixture: ComponentFixture<CheckoutpageComponent>;
+  let store: jasmine.SpyObj<Store>;
+
+  let cart = [
+    {
+      id: 16,
+      buyCount: 1,
+      price: 1.99,
+      title: 'Apple',
+      image: 'https://flexyshopimages.pilexlaflex.com/images/16.webp',
+    },
+    {
+      id: 11,
+      buyCount: 1,
+      price: 1899.99,
+      title: 'Annibale Colombo Bed',
+      image: 'https://flexyshopimages.pilexlaflex.com/images/11.webp',
+    },
+  ];
 
   beforeEach(async () => {
+    store = jasmine.createSpyObj('Store', ['select']);
+
     await TestBed.configureTestingModule({
       imports: [CheckoutpageComponent],
-      providers: [provideRouter([]), provideStore({ cart: cartReducer })],
+      providers: [provideRouter([]), { provide: Store, useValue: store }],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(CheckoutpageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('Make sure the checkoutPage is correctly rendered', () => {
-    // write test
+    fixture = TestBed.createComponent(CheckoutpageComponent);
+    component = fixture.componentInstance;
+    store.select.and.returnValue(of({ items: cart }));
+    fixture.detectChanges();
+
+    const invoiceTable = fixture.debugElement.query(By.css('#invoiceTable'));
+    const emptyCart = fixture.debugElement.query(By.css('#emptyCart'));
+    expect(invoiceTable).toBeTruthy();
+    expect(emptyCart).toBeFalsy();
   });
 
   it('If no items are present in the cart, display message', () => {
-    // write test
+    fixture = TestBed.createComponent(CheckoutpageComponent);
+    component = fixture.componentInstance;
+    store.select.and.returnValue(of({ items: [] }));
+    fixture.detectChanges();
+    const invoiceTable = fixture.debugElement.query(By.css('#invoiceTable'));
+    const emptyCart = fixture.debugElement.query(By.css('#emptyCart'));
+
+    expect(invoiceTable).toBeFalsy();
+    expect(emptyCart).toBeTruthy();
   });
 });
