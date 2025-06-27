f1a6f8345280a66c4bc8da89e514ae9b682373e2
Elie Soueid <eliegebransoued@gmail.com>
2025-06-27 13:07:38 +0200

    fix tests

--- src/app/views/cartview/cartcontent/cartcontent.component.spec.ts
+++ src/app/views/cartview/cartcontent/cartcontent.component.spec.ts
@@ -1,19 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Store } from '@ngrx/store';
 import { CartcontentComponent } from './cartcontent.component';
+import { cart } from '../../../mockData';
+import { of } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('CartcontentComponent', () => {
   let component: CartcontentComponent;
   let fixture: ComponentFixture<CartcontentComponent>;
+  let store: jasmine.SpyObj<Store>;
 
   beforeEach(async () => {
+    store = jasmine.createSpyObj('Store', ['select']);
     await TestBed.configureTestingModule({
-      imports: [CartcontentComponent]
-    })
-    .compileComponents();
+      imports: [CartcontentComponent],
+      providers: [provideHttpClient(), { provide: Store, useValue: store }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CartcontentComponent);
     component = fixture.componentInstance;
+    store.select.and.returnValue(of({ items: cart }));
+
     fixture.detectChanges();
   });
 
